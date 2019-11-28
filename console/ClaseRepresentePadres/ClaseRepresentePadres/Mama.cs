ba3cd3a2d2ad60af8cb72471fd8fdb20009fc26c
jorge <jorgearturo_257@hotmail.com>
2019-11-27 21:09:27 -0600

    PVT12 Clase represente padres

---
+++ console/ClaseRepresentePadres/ClaseRepresentePadres/Mama.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseRepresentePadres
+{
+    class Mama
+    {
+        string colorDeOjos = "Cafés";
+        string RasgozFaciales = "mejillas";
+
+
+    void reir()
+        {
+            Boolean reir = true;
+        }
+    void comer()
+        {
+            bool comer = true;
+        }       
+        void trabajar()
+        {
+            bool trabajara = true;
+        }
+        void conducir()
+        {
+            bool conduce = true;
+        }
+    }
+
+    class Papa
+    {
+        string cabello = "chino";
+        string estatura = "1.72";
+        string velloFacial = "barba y bigote";
+
+        void socializar()
+        {
+            bool socializar = true;
+        }
+        void trabajar()
+        {
+            bool trabajar = true;
+        }
+        void conducir()
+        {
+            bool conduce = true;
+        }
+    }
+
+    
+}
